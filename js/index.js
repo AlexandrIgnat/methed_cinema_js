7a3712bdb61173bef9b2f453b51139b501db7897
Aleksandr Ignatev <ignatyevaleksandr.1992@mail.ru>
2021-12-09 21:13:43 +0300

    add burger active

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+  import slideMenu from "./menu.js";
+  slideMenu({
+   openBtn : '.header__burger-btn', 
+   menu : '.navigation', 
+   classActive : 'navigation_active', 
+   closeTriger : '.navigation__close'
+  });
---
+++ js/menu.js
@@ -0,0 +1,29 @@
+
+const openMenu = (nav, active) => {
+    nav.classList.add(active);
+};
+
+const closeMenu  = (nav, active) => {
+    nav.classList.remove(active);
+}
+
+const slideMenu = ({openBtn, menu, classActive, closeTriger}) => {
+const burgerBtn = document.querySelector(openBtn);
+const navigation = document.querySelector(menu);
+const navigationClose = document.querySelectorAll(closeTriger)
+
+    burgerBtn.addEventListener('click', () => {
+        openMenu(navigation, classActive);
+    }
+    )
+
+    navigationClose.forEach((item) => {
+        item.addEventListener('click', () => {
+            closeMenu(navigation, classActive)
+        })
+    })
+
+
+};
+
+export default slideMenu;
